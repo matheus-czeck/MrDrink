b672029ba691e474db139c82d947ebbe63f4e5ab
matheus-czeck <matheusczeckcz@gmail.com>
2025-01-13 15:13:33 -0300

    att front

--- Mrdrink-front/src/app/app.routes.ts
+++ Mrdrink-front/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './modules/login/login.component'
-import { Component } from '@angular/core';
-
 export const routes: Routes = [
 {
-    path:"", component: LoginComponent
+    path:"login", component: LoginComponent
+},
+{
+ 
+    path:"**", redirectTo: "login"
+
 }
 
 ];
