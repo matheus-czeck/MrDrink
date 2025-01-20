3f93af424cbc2c4f92568eb97af0aeb33e0e4688
matheus-czeck <matheusczeckcz@gmail.com>
2025-01-20 16:14:33 -0300

    correção login

--- Mrdrink-back/src/services/service/auth.service.ts
+++ Mrdrink-back/src/services/service/auth.service.ts
@@ -2,7 +2,7 @@ import { User } from '../../models/user'
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-const SECRET_KEY = process.env.JWT_SECRET || 'default-secret0;'
+const SECRET_KEY = process.env.JWT_SECRET || 'default-secret';
 
 export const loginUser = async (userName: string, password: string) => {
     const user = await User.findOne({ where: { userName }})
@@ -11,6 +11,7 @@ export const loginUser = async (userName: string, password: string) => {
         throw new Error("Usuário não encontrado");
 
     }
+
         const isPassowordValid = await bcrypt.compare(password, user.password);
 
     if(!isPassowordValid){
@@ -19,6 +20,7 @@ export const loginUser = async (userName: string, password: string) => {
     }
 
     const token = jwt.sign({ id: user.id, userName: user.userName}, SECRET_KEY, { expiresIn: "1h"});
+    console.log("Token gerado: ", token)
     return {token, user}
 
 }
