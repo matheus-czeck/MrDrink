9fc6f7db90c2ff72a4eeefd6067caeb9ddea38bc
matheus-czeck <matheusczeckcz@gmail.com>
2025-01-23 17:07:47 -0300

    Init dashboard

--- Mrdrink-front/src/app/modules/dashboard/dashboard.component.ts
+++ Mrdrink-front/src/app/modules/dashboard/dashboard.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [NzToolTipModule, NzMenuModule , NzIconModule , NzButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
+  PeopleName: string = "Gustavo"
+  isCollapsed = false;
+
+  toggleCollapsed(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
+
+
