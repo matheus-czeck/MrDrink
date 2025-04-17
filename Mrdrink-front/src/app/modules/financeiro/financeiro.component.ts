64ee6e0aceeaf0e4c6da1145d5c1779dc841d722
matheus-czeck <matheusczeckcz@gmail.com>
2025-04-17 17:13:04 -0300

    init financeiro

--- Mrdrink-front/src/app/modules/financeiro/financeiro.component.ts
+++ Mrdrink-front/src/app/modules/financeiro/financeiro.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MainTemplate } from '../mainTemplate/mainTemplate.component';
+import { GetInformations } from '../../services/serviceEvent/getInformations.service';
 
 @Component({
   selector: 'app-financeiro',
@@ -7,6 +8,36 @@ import { MainTemplate } from '../mainTemplate/mainTemplate.component';
   templateUrl: './financeiro.component.html',
   styleUrl: './financeiro.component.scss'
 })
-export class FinanceiroComponent {
+export class FinanceiroComponent implements OnInit {
+
+  constructor(
+    private getInformations: GetInformations
+  ) {}
+
+  ngOnInit(){
+    this.getValues()
+
+  }
+
+  budgetValeu: [] = []
+  totalBudget = 0
+
+  getValues(){
+    this.getInformations.getEvents().subscribe({
+      next:(data)=>{
+
+        this.budgetValeu = data.confirmedEvent.map((
+          value:{value: number}) => Number(value.value
+        ))
+        this.totalBudget= this.budgetValeu.reduce((sum, sumValue) => sum + sumValue, 0)
+        
+        console.log( this.totalBudget)
+
+      }, error:(err)=>{
+        console.log(err)
+      }
+    })
+
+  }
 
 }
